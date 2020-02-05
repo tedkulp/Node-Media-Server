bab24c27b0ee22d51bb5d093791b60fa10a49ff4
Ted Kulp <ted@tedkulp.com>
2020-02-05 09:04:53 -0500

    feat: flv, mkv recording. add recording related events.

--- node_trans_session.js
+++ node_trans_session.js
@@ -10,11 +10,35 @@ const { spawn } = require('child_process');
 const dateFormat = require('dateformat');
 const mkdirp = require('mkdirp');
 const fs = require('fs');
+const context = require("./node_core_ctx");
+
+const clean = (obj) => {
+  let newObj = {};
+
+  for (const propName in obj) {
+    if (obj[propName] !== null && obj[propName] !== undefined) {
+      newObj[propName] = obj[propName];
+    }
+  }
+
+  return newObj;
+};
+
+const stringify = (obj) => {
+  return Object.keys(obj).map((k) => `${k}: ${obj[k]}`).join(',');
+};
 
 class NodeTransSession extends EventEmitter {
   constructor(conf) {
     super();
     this.conf = conf;
+    this.mp4Filename = null;
+    this.flvFilename = null;
+    this.mkvFilename = null;
+  }
+
+  isRecording() {
+    return this.mp4Filename || this.flvFilename || this.mkvFilename;
   }
 
   run() {
@@ -36,9 +60,21 @@ class NodeTransSession extends EventEmitter {
     if (this.conf.mp4) {
       this.conf.mp4Flags = this.conf.mp4Flags ? this.conf.mp4Flags : '';
       let mp4FileName = dateFormat('yyyy-mm-dd-HH-MM') + '.mp4';
-      let mapMp4 = `${this.conf.mp4Flags}${ouPath}/${mp4FileName}|`;
+      let mapMp4 = this.mp4Filename = `${this.conf.mp4Flags}${ouPath}/${mp4FileName}|`;
       mapStr += mapMp4;
-      Logger.log('[Transmuxing MP4] ' + this.conf.streamPath + ' to ' + ouPath + '/' + mp4FileName);
+      // Logger.log('[Transmuxing MP4] ' + this.conf.streamPath + ' to ' + ouPath + '/' + mp4FileName);
+    }
+    if (this.conf.flv) {
+      this.conf.flvFlags = this.conf.flvFlags ? this.conf.flvFlags : '';
+      let flvFileName = dateFormat('yyyy-mm-dd-HH-MM') + '.flv';
+      let mapFlv = this.flvFilename = `${this.conf.flvFlags}${ouPath}/${flvFileName}|`;
+      mapStr += mapFlv;
+    }
+    if (this.conf.mkv) {
+      this.conf.mkvFlags = this.conf.mkvFlags ? this.conf.mkvFlags : '';
+      let mkvFileName = dateFormat('yyyy-mm-dd-HH-MM') + '.mkv';
+      let mapMkv = this.mkvFilename = `${this.conf.mkvFlags}${ouPath}/${mkvFileName}|`;
+      mapStr += mapMkv;
     }
     if (this.conf.hls) {
       this.conf.hlsFlags = this.conf.hlsFlags ? this.conf.hlsFlags : '';
@@ -54,6 +90,11 @@ class NodeTransSession extends EventEmitter {
       mapStr += mapDash;
       Logger.log('[Transmuxing DASH] ' + this.conf.streamPath + ' to ' + ouPath + '/' + dashFileName);
     }
+    const recordingFiles = clean({
+      mp4: this.mp4Filename && this.mp4Filename.slice(0, -1),
+      mkv: this.mkvFilename && this.mkvFilename.slice(0, -1),
+      flv: this.flvFilename && this.flvFilename.slice(0, -1),
+    });
     mkdirp.sync(ouPath);
     let argv = ['-y', '-fflags', 'nobuffer', '-i', inPath];
     Array.prototype.push.apply(argv, ['-c:v', vc]);
@@ -67,6 +108,11 @@ class NodeTransSession extends EventEmitter {
       Logger.ffdebug(e);
     });
 
+    if (this.isRecording()) {
+      context.nodeEvent.emit("preRecord", this.conf.streamPath, recordingFiles);
+      Logger.log('[Start Recording] filename(s): ' + stringify(recordingFiles));
+    }
+
     this.ffmpeg_exec.stdout.on('data', (data) => {
       Logger.ffdebug(`FF输出：${data}`);
     });
@@ -78,6 +124,12 @@ class NodeTransSession extends EventEmitter {
     this.ffmpeg_exec.on('close', (code) => {
       Logger.log('[Transmuxing end] ' + this.conf.streamPath);
       this.emit('end');
+
+      if (this.isRecording()) {
+        context.nodeEvent.emit("doneRecord", this.conf.streamPath, recordingFiles);
+        Logger.log('[End Recording] filename(s): ' + stringify(recordingFiles));
+      }
+
       fs.readdir(ouPath, function (err, files) {
         if (!err) {
           files.forEach((filename) => {
